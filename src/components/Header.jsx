91b7f387390f176cec0def546ea53ab69f7b228e
Cody Touchet <cody.touchet@ibm.com>
2020-10-14 13:22:20 -0500

    keeper-app-v1

---
+++ src/components/Header.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const HeaderTitle = () => {
+    return (
+        <header>
+            <h1>Keeper</h1>
+        </header>
+    );
+};
+
+export default HeaderTitle;
---
+++ src/components/Note.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Note = () => {
+    return (
+        <div className="note">
+            <h1>Title Here</h1>
+            <p>Text here</p>
+        </div>
+    );
+};
+
+export default Note;
